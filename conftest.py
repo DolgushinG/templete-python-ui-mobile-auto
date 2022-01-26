3e2d16cc0488983b9a0d37d5ae97f705ad7adaf3
g.dolgushin <g.dolgushin@mcplat.ru>
2022-01-26 10:59:51 +0300

    added recorder

--- conftest.py
+++ conftest.py
@@ -45,3 +45,36 @@ def driver(request):
         request.cls.driver.hide_keyboard()
     request.cls.driver.quit()
 
+# set up a hook to be able to check if a test has failed
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
+# check if a test has failed
+@pytest.fixture(scope="function", autouse=True)
+def test_failed_check(request):
+    yield
+    # request.node is an "item" because we use the default
+    # "function" scope
+    if request.node.rep_setup.failed:
+        print("setting up a test failed!", request.node.nodeid)
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            driver = request.cls.driver
+            take_screenshot(driver, request.node.nodeid)
+            print("executing test failed", request.node.nodeid)
+
+
+# make a screenshot with a name of the test, date and time
+def take_screenshot(driver, nodeid):
+    time.sleep(1)
+    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png'.replace("/", "_").replace("::", "__")
+    driver.save_screenshot(f'output/{file_name}')
